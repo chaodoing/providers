06bc2288b7933d598e2a3a5840cb8f0f58b399c3
何烨霖 <chaodoing@hotmail.com>
2022-07-23 17:14:53 +0800

    修改加密方法

--- util/encrypt.go
+++ util/encrypt.go
@@ -22,8 +22,14 @@ func Md5(value string) string {
 func UUID() string {
 	f, _ := os.OpenFile("/dev/urandom", os.O_RDONLY, 0)
 	b := make([]byte, 16)
-	f.Read(b)
-	f.Close()
+	_, err := f.Read(b)
+	if err != nil {
+		return ""
+	}
+	err = f.Close()
+	if err != nil {
+		return ""
+	}
 	return fmt.Sprintf("%x-%x-%x-%x-%x", b[0:4], b[4:6], b[6:8], b[8:10], b[10:])
 }
 
